8d8a014cdb7116879dbfae1cf1ca6ffd88f4e47d
AntoninESSEUL <anto.ess85@gmail.com>
2024-01-03 11:24:35 +0100

    test

--- odoo_python/odoo01.py
+++ odoo_python/odoo01.py
@@ -1,31 +1,29 @@
 import xmlrpc.client
 
-def Connect(ip='localhost', port=8069):
+def Connect(ip='localhost', port=8069, password='2000'):
     # Informations de connexion à Odoo
     url = 'http://{}:{}/'.format(ip, port)
     db = 'demo'
     username = 'emilienqr@gmail.com'
-    password = '2000'
-
+    
     # Établir une connexion
     common = xmlrpc.client.ServerProxy('{}xmlrpc/2/common'.format(url))
     uid = common.authenticate(db, username, password, {})
 
     # Vérifier si l'authentification a réussi
     if uid:
-        print('Authentification réussie. UID: {}'.format(uid))
+        print('Connection réussie.')
     else:
-        print('Échec de l\'authentification.')
-        return
-
-    # Afficher l'adresse URL de connexion
-    print('Adresse URL de connexion à Odoo:', url)
-
-    # Récupérer la version d'Odoo
-    version = common.version()
-    print('Version d\'Odoo:', version.get('server_version'))
+        print('Échec de la connexion.')
 
 if __name__ == '__main__':
-    # Utilisation par défaut avec localhost et port 8069
+    # Utilisation par défaut avec localhost, port 8069 et mot de passe vide
     Connect()
+
+    # Vous pouvez également spécifier une adresse IP différente et/ou un port
+    # Connect(ip='192.168.1.100', port=8070)
+
+    # Utilisation avec un mot de passe spécifié
+    # Connect(password='votre_mot_de_passe')
+
 
