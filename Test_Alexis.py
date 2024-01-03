14a381de909fcf03081725491591d35f428beac9
Alex-Tech22 <alexis.leroy14250@gmail.com>
2024-01-03 14:39:09 +0100

    update

--- Test_Alexis.py
+++ Test_Alexis.py
@@ -1,12 +1,23 @@
-# programme calcule de hauteur
+from pathlib import Path
+import tkinter as tk
 
-nb_boite = int(input("Entrez le nombre de boite :"))
-if nb_boite < 1:
-    nb_boite = 1
-elif nb_boite > 100:
-    nb_boite = 100
-print (nb_boite)
-nb_couche = int(input("Entrez le nombre de boite par couche (4,5,6 ou 9):"))
-h_couche = nb_boite/nb_couche
-h = h_couche*0.25
-print("la hauteur est de {}m".format(h))
+#============================================================
+
+class App(tk.Tk):
+    """Application GUI in TKinter"""
+    def __init__(self):
+        """Application constructor (heritage=Tk object)"""
+        super().__init__()
+        self.screen_h = 800
+        self.screen_v = self.winfo_screenheight()
+        self.screen_x = 50
+        self.screen_y = 0
+        geometry = str(self.screen_h)+"x"+str(self.screen_v)+"+"+str(self.screen_x)+"+"+str(self.screen_y)
+
+        
+
+#============================================================
+        
+if __name__ == "__main__":
+    myApp = App()
+    myApp.mainloop()
