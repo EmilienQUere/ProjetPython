d3073adc6ab85e7ece389e5d5edbca21e3977a75
EmilienQUere <emilienqr@gmail.com>
2024-01-03 17:28:04 +0100

    Remise en place de Antonin

--- ODOO/Dev_python/TESTS_ODOO.py
+++ ODOO/Dev_python/TESTS_ODOO.py
@@ -1,23 +1,14 @@
 import xmlrpc.client
 
-<<<<<<< HEAD
-# Utilisation de la fonction Company() pour récupérer l'identifiant d'une entreprise spécifique
-=======
-
->>>>>>> dc584bd42b703a7987ba115fb8fdae7a27be0d59
+#Déclaration des variables générales
 url = 'http://172.31.11.13:8069'
 db = 'demo'
 username = 'emilienqr@gmail.com'
 password = '2000'
 company_name = 'Barbak'
 
-<<<<<<< HEAD
-def connect(url, db, username, password)  
-=======
-
+#Déclaration de la fonction
 def connect(url, db, username, password):
-   
->>>>>>> dc584bd42b703a7987ba115fb8fdae7a27be0d59
     try:
         common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
         uid = common.authenticate(db, username, password, {})
@@ -58,11 +49,8 @@ def Company(models, db, uid, password, company_name):
     except Exception as e:
         print(f"Erreur lors de la recherche de l'entreprise : {e}")
         return None
-
-<<<<<<< HEAD
-=======
+    
 # Utilisation de la fonction Company() pour récupérer l'identifiant d'une entreprise spécifique
->>>>>>> dc584bd42b703a7987ba115fb8fdae7a27be0d59
 odoo_models, odoo_connection = connect(url, db, username, password)
 if odoo_models and odoo_connection:
     print("Connexion réussie à Odoo")
