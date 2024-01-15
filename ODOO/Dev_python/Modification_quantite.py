e6295bd026a011cbccd76bd347cea8e2508131fd
AntoninESSEUL <anto.ess85@gmail.com>
2024-01-15 17:34:44 +0100

    page logistique en cours

--- ODOO/Dev_python/Modification_quantite.py
+++ ODOO/Dev_python/Modification_quantite.py
@@ -1,4 +1,5 @@
 import xmlrpc.client
+from datetime import datetime
 
 def connect(url, db, username, password):
     try:
@@ -7,50 +8,41 @@ def connect(url, db, username, password):
         
         if uid:
             models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object")
-            return models, xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object")
+            return models, uid
         else:
             print("Connexion échouée : Authentification impossible")
-            return None
+            return None, None
     except Exception as e:
         print(f"Erreur de connexion : {e}")
-        return None
+        return None, None
 
-def update_stock(self):
-<<<<<<< HEAD
-        article_default_code = self.entry_ref.get()
-        new_quantity = max(0, int(self.entry_quantity.get()))
- 
-        
-=======
-        article_default_code = "[J87]"
-        new_quantity = 5
-        # Connexion à Odoo
+def modif_qty(order_id, new_quantity):
+    try:
+        # Connexion XML-RPC à Odoo
         url = "http://172.31.11.13:8069"
         db = "demo2"
-        username = "administrateur"
-        password = "2000"
->>>>>>> 951ddd157c107795d9e4e45ddea45ce2b14cf19f
- 
-        common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-        uid = common.authenticate(db, username, password, {})
-        models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
- 
-        # Recherche de l'ID de l'article par la référence interne
-        article_id = models.execute_kw(db, uid, password, 'product.template', 'search', [[('default_code', '=', article_default_code)]])
-        
-        if article_id:
-            # Rechercher les entrées du modèle stock.quant associées à l'article
-            stock_entries = models.execute_kw(db, uid, password, 'stock.quant', 'search_read', [
-                [('product_id', '=', article_id[0])]
-            ])
- 
-            
+        username = "your_username"
+        password = "your_password"
 
-if __name__=="__main__":
+        models, uid = connect(url, db, username, password)
+
+        if models and uid:
+            result = models.execute_kw(db, uid, 'your_password', 'product.product', 'write',
+                [[order_id], {'qty_available': new_quantity}]
+            )
+
+            if result:
+                print(f"Quantité dans le stock de l'article avec l'ID {order_id} modifiée avec succès.")
+            else:
+                print(f"La modification de la quantité dans le stock de l'article avec l'ID {order_id} a échoué.")
+    except Exception as e:
+        print(f"Erreur lors de la modification de la quantité dans le stock : {e}")
+
+
+if __name__ == "__main__":
     # Connexion à Odoo
-        url = "http://localhost:8069"
-        db = "demo2"
-        username = "administrateur"
-        password = "2000"
-
-        
+    url = "http://localhost:8069"
+    db = "demo2"
+    username = "administrateur"
+    password = "2000"
+    modif_qty(51, 24)
