64be5a2adb88669b942f18437315f4495fb36d95
AntoninESSEUL <anto.ess85@gmail.com>
2024-03-12 17:24:32 +0100

    k

--- Desktop_Tkinter/Dev_python/Page_Login.py
+++ Desktop_Tkinter/Dev_python/Page_Login.py
@@ -116,7 +116,7 @@ class App(tk.Tk):
         self.mdp_to_test = self.mdp_entry.get()
 
         try:
-            socket.setdefaulttimeout(5)
+            socket.setdefaulttimeout(2)
             common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(ip_add))
             uid = common.authenticate('demo2', self.user_to_test, self.mdp_to_test, {})
 
