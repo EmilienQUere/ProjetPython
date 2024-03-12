93b34047c63e7fc4c2de8b7a0473fa1693ded629
AntoninESSEUL <anto.ess85@gmail.com>
2024-03-12 17:25:36 +0100

    Etat final

--- Desktop_Tkinter/Dev_python/Page_Login.py
+++ Desktop_Tkinter/Dev_python/Page_Login.py
@@ -179,7 +179,7 @@ class App(tk.Tk):
                 print("Ping échoué")
                 self.connexion_status.config(text="Échec de connexion", foreground="red")
             # Attendez 5 secondes avant la prochaine vérification
-            time.sleep(5)
+            time.sleep(2)
 
 # Boucle principale
 if __name__ == "__main__":
