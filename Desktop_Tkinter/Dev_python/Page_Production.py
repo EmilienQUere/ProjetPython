f15cd9e6ebcbb75ec831223361170b6eb41f5db6
AntoninESSEUL <anto.ess85@gmail.com>
2024-03-12 11:21:43 +0100

    modif quantité superieur à la quantité demandé

--- Desktop_Tkinter/Dev_python/Page_Production.py
+++ Desktop_Tkinter/Dev_python/Page_Production.py
@@ -5,6 +5,7 @@ from tkinter import ttk, messagebox, simpledialog
 from PIL import Image, ImageTk
 from datetime import datetime
 import xmlrpc.client
+import tkinter.simpledialog
 
 COULEUR_DE_FOND = "#{:02x}{:02x}{:02x}".format(52, 73, 74)
 image_path = os.path.abspath("BARBAK.png")
@@ -122,22 +123,24 @@ class AppProd(tk.Tk):
         try:
             self.of_records = xmlrpc.client.ServerProxy(f"{'http://172.31.11.13:8069'}/xmlrpc/2/object").execute_kw(
                 'demo2', 9 , self.mdp_to_test , 'mrp.production', 'search_read',
-                [[('state', 'not in', ['done', 'cancel'])]],
+                [[('state', 'not in', ['done', 'cancel', 'to_close'])]],
                 {'fields': ['product_id', 'name', 'product_qty', 'date_planned_start', 'qty_producing', 'state']}
             )
 
             table_data = []
             for of in self.of_records:
-                article_name = of.get('product_id')[1] if of.get('product_id') else ''
-                OF_ID = of.get('id')
-                OF_Name = of.get('name')
-                Quantity = int(of.get('product_qty'))
-                quantity_produced = int(of.get('qty_producing'))
-                date = datetime.strptime(of.get('date_planned_start'), '%Y-%m-%d %H:%M:%S').strftime('%d/%m/%y %H:%M:%S')
-                etat_OF = of.get('state')
+                # Ajoutez une condition pour vérifier si l'état de l'OF est "to_close"
+                if of.get('state') != 'to_close':
+                    article_name = of.get('product_id')[1] if of.get('product_id') else ''
+                    OF_ID = of.get('id')
+                    OF_Name = of.get('name')
+                    Quantity = int(of.get('product_qty'))
+                    quantity_produced = int(of.get('qty_producing'))
+                    date = datetime.strptime(of.get('date_planned_start'), '%Y-%m-%d %H:%M:%S').strftime('%d/%m/%y %H:%M:%S')
+                    etat_OF = of.get('state')
 
-                table_data.append((article_name, OF_Name, date, Quantity, quantity_produced, etat_OF))
-                self.id_of_mapping[OF_Name] = OF_ID
+                    table_data.append((article_name, OF_Name, date, Quantity, quantity_produced, etat_OF))
+                    self.id_of_mapping[OF_Name] = OF_ID
 
             self.update_table(table_data)
 
@@ -150,7 +153,6 @@ class AppProd(tk.Tk):
         if item and self.modif_en_cours:
             colonne = self.tableau.identify_column(event.x)
             id_colonne = self.tableau.column(colonne)["id"]
-            indice_colonne = self.tableau["columns"].index(id_colonne)
             values = self.tableau.item(item, "values")
 
             if values:
@@ -175,18 +177,47 @@ class AppProd(tk.Tk):
             style = ttk.Style()
             style.configure("Modifier.TButton", background=COULEUR_DE_FOND)
     
+    def saisir_quantite(self, qty_to_produce):
+        while True:
+            nouvelle_valeur = tkinter.simpledialog.askinteger("Modifier Valeur", f"La nouvelle quantité produite ne peut pas dépasser {qty_to_produce}. Entrez une nouvelle quantité :", initialvalue=qty_to_produce)
+            if nouvelle_valeur is None:
+                return None  # L'utilisateur a appuyé sur Annuler, donc sortez de la boucle
+            elif isinstance(nouvelle_valeur, int):
+                return nouvelle_valeur  # La valeur est une entière, sortez de la boucle
+            else:
+                messagebox.showerror("Erreur", "Veuillez entrer une valeur entière.")
+
+
     def modif_qty(self, order_id, new_quantity):
         try:
-            result = xmlrpc.client.ServerProxy(f"{'http://172.31.11.13:8069'}/xmlrpc/2/object").execute_kw(
-                'demo2', 9, self.mdp_to_test , 'mrp.production', 'write',
-                [[order_id], {'qty_producing': new_quantity}]
+            # Récupérez la quantité à produire pour l'OF
+            of_record = xmlrpc.client.ServerProxy(f"{'http://172.31.11.13:8069'}/xmlrpc/2/object").execute_kw(
+                'demo2', 9, self.mdp_to_test, 'mrp.production', 'read',
+                [[order_id], ['product_qty']]
             )
-            if result:
-                print(f"Quantité produite dans l'ordre de fabrication avec l'ID {order_id} modifiée avec succès.")
+            qty_to_produce = of_record[0]['product_qty']
+
+            # Vérifiez si la nouvelle quantité produite est inférieure ou égale à la quantité à produire
+            if 0 <= new_quantity <= qty_to_produce:
+                result = xmlrpc.client.ServerProxy(f"{'http://172.31.11.13:8069'}/xmlrpc/2/object").execute_kw(
+                    'demo2', 9, self.mdp_to_test, 'mrp.production', 'write',
+                    [[order_id], {'qty_producing': new_quantity}]
+                )
+                if result:
+                    print(f"Quantité produite dans l'ordre de fabrication avec l'ID {order_id} modifiée avec succès.")
+                else:
+                    print(f"La modification de la quantité produite dans l'ordre de fabrication avec l'ID {order_id} a échoué.")
             else:
-                print(f"La modification de la quantité produite dans l'ordre de fabrication avec l'ID {order_id} a échoué.")
+                print(f"La nouvelle quantité produite dépasse la quantité à produire pour l'ordre de fabrication avec l'ID {order_id}.")
+                # Demandez à l'utilisateur de saisir une nouvelle quantité valide
+                nouvelle_quantite = self.saisir_quantite(qty_to_produce)
+                if nouvelle_quantite is not None:
+                    # Appelez récursivement modif_qty avec la nouvelle quantité saisie
+                    self.modif_qty(order_id, nouvelle_quantite)
+
         except Exception as e:
             print(f"Erreur lors de la modification de la quantité produite : {e}")
+
 
 #=========================================================================================
             
